fc053e7ded8c52be219a79c1cde663c8c3a1948e
ngprabesh <ngprabesh123@gmail.com>
2024-05-31 17:42:53 +0545

    changes: add detail page for post add styles

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('', views.posts, name='posts'),
-    # path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from .models import Post
 def posts(request):
     posts = Post.objects.filter(status=1)  
     return render(request, 'index.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
